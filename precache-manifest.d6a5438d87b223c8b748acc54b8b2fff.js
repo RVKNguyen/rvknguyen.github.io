19f77d326ec8ab990107d397b2ffa112f565be3e
raul <raulnguyen91@gmail.com>
2020-05-10 16:12:03 +0200

    v0.0.1

---
+++ precache-manifest.d6a5438d87b223c8b748acc54b8b2fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6ff72fd8e63f6c9697fd3135bf31d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53def406bafa6ba68f43",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b02aa9b5fece75341de",
+    "url": "/static/js/2.f656e2ca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f656e2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53def406bafa6ba68f43",
+    "url": "/static/js/main.1a8990e8.chunk.js"
+  },
+  {
+    "revision": "73d36aed5d55bca1c7b8",
+    "url": "/static/js/runtime-main.b441eab6.js"
+  }
+]);
